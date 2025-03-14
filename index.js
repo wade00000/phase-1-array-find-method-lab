3a8e68739b52bdf2cc9479d896e13284ce428b17
WADE <MEANTOINE12@GMAIL.COM>
2025-03-14 12:49:30 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+
+  function superbowlWin(record) {
+    const win = record.find(function(game) {
+        return game.result === "W";
+    });
+    return win ? win.year : undefined;
+}
+ 
+console.log(superbowlWin(record))
